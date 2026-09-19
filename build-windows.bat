185245386af1e36bc8b4e5bbdf0d94ea73d82e0d
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-09-20 04:58:31 +0530

    Set windows icon using cmake rc

--- build-windows.bat
+++ build-windows.bat
@@ -1,25 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Product version from first argument
-set PRODUCT_VERSION=%1
-
 rem Paths
 set EXE=build\Release\Tokri.exe
 set OUTDIR=dist\Windows
-set ICON=resources\net.surajyadav.Tokri.ico
-set PRODUCT_NAME=Tokri
 
 rem Deploy Qt
 windeployqt --dir "%OUTDIR%" "%EXE%"
 
 rem Copy exe to release (overwrite)
 copy /Y "%EXE%" "%OUTDIR%\Tokri.exe"
-
-rem Set properties
-rcedit "%OUTDIR%\Tokri.exe" ^
-  --set-icon "%ICON%" ^
-  --set-version-string "ProductName" "%PRODUCT_NAME%" ^
-  --set-product-version "%PRODUCT_VERSION%"
 
 endlocal
